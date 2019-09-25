1a9349e2d1fffff7c24048c6c186b9f1c12db5e6
Askhat <jam.askhat@gmail.com>
2019-09-26 01:22:45 +0600

    the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+  let arr = str.split("");
+  let stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] == bracketsConfig[j][0]) {
+        if (stack.length == 0) {
+          stack.push(arr[i]);
+        } else if (stack[stack.length-1] == bracketsConfig[j][1]) {
+          stack.pop();
+        } else {
+          stack.push(arr[i]);
+        }
+      } else if (arr[i] == bracketsConfig[j][1]) {
+        if (stack.length > 0) {
+          if (stack[stack.length-1] == bracketsConfig[j][0]) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true;
+  }  else {
+    return false;
+  }
 }
